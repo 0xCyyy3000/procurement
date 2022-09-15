a6b633c3ad3d513f05f6a8737dc42e635762d725
cy <cyrilschool12@gmail.com>
2022-09-15 21:23:46 +0800

    modified: Working on submitting the Requesition

--- resources/views/partials/_priority.blade.php
+++ resources/views/partials/_priority.blade.php
@@ -4,15 +4,17 @@
         <div class="icon">
             <span class="material-icons-sharp">label</span>
         </div>
-        <div class="right">
-            <h3>Priority level</h3>
-            <select class="primary" id="priority-level">
-                <option value="default">-- Choose one -- </option>
-                <option>High (Urgent)</option>
-                <option>Normal</option>
-                <option>Low (Not Urgent)</option>
-            </select>
-        </div>
+        <form action="">
+            <div class="right">
+                <h3>Priority level</h3>
+                <select class="primary" id="priority-level">
+                    <option value="default">-- Choose one -- </option>
+                    <option>High (Urgent)</option>
+                    <option>Normal</option>
+                    <option>Low (Not Urgent)</option>
+                </select>
+            </div>
+        </form>
     </div>
     <div class="item submit">
         <button type="submit" form="req-details-form" name="button">
