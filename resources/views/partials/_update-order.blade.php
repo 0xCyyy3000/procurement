ea73c2392b2ea4cf7c5c2859a6ad85934d975685
cy <cyrilschool12@gmail.com>
2022-11-04 21:12:18 +0800

    Working on selecting and viewing PurchaseOrder

---
+++ resources/views/partials/_update-order.blade.php
@@ -0,0 +1,42 @@
+<div class="order-info">
+    <link rel="stylesheet" href="{{ asset('css/update-order.css') }}">
+    <h2>Update Purchase Order</h2>
+    <div class="details">
+        <div class="detail">
+            <div class="row">
+                <span class="material-icons-sharp primary-variant">receipt_long</span>
+                <div class="right-side">
+                    <h3>
+                        <select id="purchase-orders">
+                            <option value="default">-- Please Choose one --</option>
+                        </select>
+                    </h3>
+                    <small class="text-muted">Purchased Order No.</small>
+                </div>
+            </div>
+        </div>
+        <div class="detail">
+            <div class="row">
+                <span class="material-icons-sharp primary-variant">payments</span>
+                <div class="right-side">
+                    <h3>
+                        <select id="payment">
+                            <option value="default">-- Please Choose one --</option>
+                            <option id="paid" value="paid">Paid</option>
+                            <option id="refunded" value="refunded">Refunded</option>
+                        </select>
+                    </h3>
+                    <small class="text-muted">Payment</small>
+                </div>
+            </div>
+        </div>
+        <div class="item update">
+            <div class="right">
+                <button type="button" id="update-button">
+                    <span class="material-icons-sharp">publish</span>
+                    <h3>Update</h3>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
